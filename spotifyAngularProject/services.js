bc0c7be3a4afe1492262c3c02b0e33678ac7bb1a
Xavier Meroño <x.merono@gmail.com>
2017-02-03 10:36:53 +0100

    SpotifyAngular services.js updated

--- spotifyAngularProject/services.js
+++ spotifyAngularProject/services.js
@@ -1,1 +1,11 @@
-angular.module('', [])
+angular.module('SpotifyFactories', [])
+
+.factory("SpotifyFactory", function($http) {
+
+  function searchArtist(searchQuery) {
+
+    var url = "https://api.spotify.com/v1/search?type=artist&query=<%ARTIST-NAME%>";
+    url = url.replace('<%ARTIST-NAME%>', searchQuery)
+
+    return $http.get(url)
+  }
